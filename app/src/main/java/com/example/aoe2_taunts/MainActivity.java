2e86b99ea770822e44c703ccbcf95ff24013e4f3
mani <mani3297@gmail.com>
2021-05-07 18:21:56 +0200

    created sorting functionality and increased button size the buttons.

--- app/src/main/java/com/example/aoe2_taunts/MainActivity.java
+++ app/src/main/java/com/example/aoe2_taunts/MainActivity.java
@@ -13,18 +13,21 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.Toast;
+
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
     Context context = this;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,24 +36,37 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int width = displayMetrics.widthPixels;
-
         int colm = width/250;
-        int btnAmount = 42;
-        int rowCount = btnAmount /colm;
-        if (btnAmount %colm != 0){
-            rowCount += 1;
-        }
-        Toast.makeText(this, colm + " and " + rowCount, Toast.LENGTH_SHORT).show();
+
         TableLayout mainTableLayout = findViewById(R.id.mainlayout);
 
         TableRow tableRow = new TableRow(context);
-//        TableLayout.LayoutParams tparams = (TableLayout.LayoutParams) tableRow.getLayoutParams();
         tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
         mainTableLayout.addView(tableRow);
 
-        Field[] fields=R.raw.class.getFields();
+        List<String> fields= Arrays.stream(R.raw.class.getFields())
+                .map(Field::getName).sorted((m1, m2) -> {
+                    String sub1 = m1.substring(m1.length() - 2);
+                    String sub2 = m2.substring(m2.length() - 2);
+                    if (sub1.charAt(0) == 0) {
+                        sub1 = "" + sub1.charAt(1);
+                    }
+                    if (sub2.charAt(0) == 0) {
+                        sub2 = "" + sub2.charAt(1);
+                    }
+                    int first = Integer.parseInt(sub1);
+                    int second = Integer.parseInt(sub2);
+                    if (first > second) {
+                        return 1;
+                    } else if (second > first) {
+                        return -1;
+                    }
+                    return 0;
+                })
+                .collect(Collectors.toList());
+
         int currentColumn = 0;
-        for (Field field : fields) {
+        for (String field : fields) {
             if(currentColumn == colm) {
                 tableRow = new TableRow(context);
                 tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
@@ -60,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
             currentColumn++;
 
             ImageButton newBtn = new ImageButton(context);
-            newBtn.setTag(field.getName());
-            newBtn.setImageResource(this.getResources().getIdentifier(field.getName(), "drawable", context.getPackageName()));
+            newBtn.setTag(field);
+            newBtn.setImageResource(this.getResources().getIdentifier(field, "drawable", context.getPackageName()));
             newBtn.setBackgroundResource(0);
             newBtn.setScaleType(ImageView.ScaleType.FIT_CENTER);
 
@@ -75,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
             float dpFactor = getResources().getDisplayMetrics().density;
             //determine params for buttons
             TableRow.LayoutParams params = (TableRow.LayoutParams) newBtn.getLayoutParams();
-            params.height = (int)(75 * dpFactor);
+            params.height = (int)(90 * dpFactor);
             params.weight = 1f;
             params.width = 0;
 
@@ -94,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Exception", e.getMessage());
         }
     }
-    @RequiresApi(api = Build.VERSION_CODES.M)
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void setClickableAnimation(ImageButton imgBtn)
     {
         TypedValue outValue = new TypedValue();
