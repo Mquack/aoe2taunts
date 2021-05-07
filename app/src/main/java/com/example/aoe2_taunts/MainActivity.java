03e5378e182da28f5dee1b410cbb710ac76ee8e8
mani <mani3297@gmail.com>
2021-05-07 15:34:24 +0200

    programatically added buttons, one on every row

--- app/src/main/java/com/example/aoe2_taunts/MainActivity.java
+++ app/src/main/java/com/example/aoe2_taunts/MainActivity.java
@@ -1,30 +1,28 @@
 package com.example.aoe2_taunts;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
-//import com.example.aoe2_taunts.R;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
     Context context = this;
     MediaPlayer mediaPlayer;
     int btnAmount = 42;
-
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -34,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int height = displayMetrics.heightPixels;
         int width = displayMetrics.widthPixels;
 
         int colm = width/250;
@@ -43,53 +40,46 @@ public class MainActivity extends AppCompatActivity {
             row += 1;
         }
         Toast.makeText(this, colm + " and " + row, Toast.LENGTH_SHORT).show();
-        //TableRow tRow = new TableRow(this);
-        TableRow tRow = (TableRow)findViewById(R.id.lastrow);
-        //tRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
-        ImageButton newBtn = new ImageButton(this);
-        //newBtn.setImageResource(R.drawable.my_granny);
+        TableLayout mainTableLayout = findViewById(R.id.mainlayout);
+        //TableRow tRow = findViewById(R.id.firstrow);
 
-        newBtn.setTag("my_granny");
-        //TypedValue outVal = new TypedValue();
-        //newBtn.setBackgroundResource(this.getTheme().resolveAttribute(android.R.attr.selectableItemBackgroundBorderless,outVal, true));
-        //newBtn.setBackground(android.R.attr.selectableItemBackgroundBorderless);
-        newBtn.setImageResource(R.drawable.my_granny);
-        newBtn.setBackgroundResource(0);
-        newBtn.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        //added to screen
-        tRow.addView(newBtn);
-        setClickableAnimation(newBtn);
-        newBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playSounds(v.getTag().toString());
-            }
-        });
-        //get factor to multiply with pixel value to get dp's
-        float dpFactor = getResources().getDisplayMetrics().density;
-        //determin params for buttons
-        TableRow.LayoutParams params = (TableRow.LayoutParams) newBtn.getLayoutParams();
-        params.height = (int)(75.0 * dpFactor);
-        //params.width = params.height;
-        params.weight = 1f;
-        params.width = 0;
-        params.bottomMargin = (int)(10.0 * dpFactor);
-        params.leftMargin = params.bottomMargin;
-        params.rightMargin = params.bottomMargin;
-        params.topMargin = params.bottomMargin;
+        Field[] fields=R.raw.class.getFields();
+        for (Field field : fields) {
+            TableRow rowwy = new TableRow(this);
+            TableLayout.LayoutParams tparams = (TableLayout.LayoutParams) rowwy.getLayoutParams();
+            rowwy.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+            mainTableLayout.addView(rowwy);
 
-        newBtn.setLayoutParams(params);
+            //tparams.topMargin = 10;
+
+            ImageButton newBtn = new ImageButton(this);
+            newBtn.setTag(field.getName());
+            newBtn.setImageResource(this.getResources().getIdentifier(field.getName(), "drawable", context.getPackageName()));
+            newBtn.setBackgroundResource(0);
+            newBtn.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+            //added to screen
+            rowwy.addView(newBtn);
+            setClickableAnimation(newBtn);
+            newBtn.setOnClickListener(this::playSounds);
+
+            //get factor to multiply with pixel value to get dp's
+            float dpFactor = getResources().getDisplayMetrics().density;
+            //determine params for buttons
+            TableRow.LayoutParams params = (TableRow.LayoutParams) newBtn.getLayoutParams();
+            params.height = (int)(75 * dpFactor);
+            params.weight = 1f;
+            params.width = 0;
+
+            newBtn.setLayoutParams(params);
+        }
+
+
     }
-    public void playSounds(String audioTag) {
-        //Toast.makeText(this,  "pressed", Toast.LENGTH_SHORT).show();
-        //Log.d("GETID", view.getTag().toString());
-
-
-        //get audio id with the tag.
+    public void playSounds(View view) {
         Resources res = context.getResources();
-        int soundId = res.getIdentifier(audioTag, "raw", context.getPackageName());
+        int soundId = res.getIdentifier(view.getTag().toString(), "raw", context.getPackageName());
 
-        //try to play audio file
         mediaPlayer = MediaPlayer.create(context, soundId);
         try{
             if (mediaPlayer.isPlaying()){
@@ -106,15 +96,7 @@ public class MainActivity extends AppCompatActivity {
     {
         TypedValue outValue = new TypedValue();
         this.getTheme().resolveAttribute(
-                android.R.attr.selectableItemBackgroundBorderless, outValue, true);
+                android.R.attr.actionBarItemBackground, outValue, true);
         imgBtn.setForeground(getDrawable(outValue.resourceId));
-    }
-    public static void btnEffect(View button){
-        button.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return false;
-            }
-        });
     }
 }
